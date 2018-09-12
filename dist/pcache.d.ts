e7e3888b7e26e2a520af2251d35cc32b7cbb3fd0
pfitzer <michael@mp-development.de>
2018-09-12 18:23:37 +0200

    changed to typescript

---
+++ dist/pcache.d.ts
@@ -0,0 +1,18 @@
+declare class Pcache {
+    private t;
+    private storage;
+    constructor(t: number);
+    set: (k: any, v: any, t: number) => void;
+    get: (k: any) => any;
+    del: (k: any) => void;
+    /**
+     *
+     */
+    clearAll: () => void;
+    /**
+     *
+     * @returns {Array}
+     */
+    keys: () => string[];
+}
+export { Pcache };
---
+++ dist/pcache.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var now = function () { return new Date().getTime(); };
+var Store = /** @class */ (function () {
+    function Store() {
+    }
+    return Store;
+}());
+var Pcache = /** @class */ (function () {
+    function Pcache(t) {
+        var _this = this;
+        this.set = function (k, v, t) {
+            var e;
+            if (!isNaN(t)) {
+                e = t * 1000 + now();
+            }
+            else if (!isNaN(_this.t)) {
+                e = _this.t + now();
+            }
+            _this.storage[k] = { value: v, expire: e };
+        };
+        this.get = function (k) {
+            if (!(k in _this.storage)) {
+                return null;
+            }
+            var v = _this.storage[k].value;
+            if (null === _this.storage[k].expire || _this.storage[k].expire >= now()) {
+                return v;
+            }
+            _this.del(k);
+            return null;
+        };
+        this.del = function (k) {
+            delete _this.storage[k];
+        };
+        /**
+         *
+         */
+        this.clearAll = function () {
+            _this.storage = {};
+        };
+        /**
+         *
+         * @returns {Array}
+         */
+        this.keys = function () {
+            var s = _this.storage;
+            Object.keys(_this.storage).forEach(function (i) {
+                if (s[i].expire <= now()) {
+                    delete s[i];
+                }
+            });
+            _this.storage = s;
+            return Object.keys(_this.storage);
+        };
+        this.t = t;
+        this.storage = {};
+    }
+    return Pcache;
+}());
+exports.Pcache = Pcache;
